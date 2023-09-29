1d9984eee879ceef9494807e8f4a6cf945e3dc74
Ilya Filatov <71808745+ilya-filatov-94@users.noreply.github.com>
2023-09-29 23:13:39 +0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,44 @@
-import {configureStore} from '@reduxjs/toolkit';
-import reducerAuth from './authSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import reducerAuth from "./authSlice";
+import reducerTheme from "./themeSlice";
 
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
-const store =  configureStore({
-    reducer: {
-        reducerAuth: reducerAuth
-    }
+const rootReducer = combineReducers({
+  reducerAuth: reducerAuth,
+  reducerTheme: reducerTheme,
 });
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["reducerAuth", "reducerTheme"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
