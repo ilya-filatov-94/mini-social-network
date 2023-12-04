e4be18283f28551a6531cd9fd27b04dd3a375a62
Filatov Ilya <ilya-filatov-1994@mail.ru>
2023-12-05 00:09:14 +0300

    updated throttle wrapper

--- client/src/helpers/throttle.ts
+++ client/src/helpers/throttle.ts
@@ -1,13 +1,19 @@
 export function throttle<Fn extends (...args: any[]) => any>(callback: Fn, delay: number) {
     let isThrottled = false;
+    let savedArgs: any;
     function wrapper(...args: any[]) {
         if (isThrottled) {
+            savedArgs = args;
             return;
         }
         callback.apply(null, args);
         isThrottled = true;
         setTimeout(() => {
             isThrottled = false;
+            if (savedArgs) {
+                wrapper.apply(null, savedArgs);
+                savedArgs = null;
+            }
         }, delay);
     }
 
